45c6e5e109d69ead384aa18f9305df28e736df8f
taka2 <taka23131@gmail.com>
2020-11-28 16:41:48 +0900

    四則演算ができるようになった

--- test.sh
+++ test.sh
@@ -18,6 +18,9 @@ assert() {
 
 assert 0 0
 assert 42 42
-assert 40 "   195 +      37 - 150+65-     107 "
+assert 40 "195 +      37 - 150+65-     107 "
+assert 4 "(3+5)/2"
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
 
 echo OK
